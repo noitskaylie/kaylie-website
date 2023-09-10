250bd5d54b59215b60f0197a20a6ee4d09bad555
noitskaylie <144586596+noitskaylie@users.noreply.github.com>
2023-09-10 01:25:09 -0700

    add bb link

--- gen.py
+++ gen.py
@@ -6,7 +6,8 @@ ROOT = '/'
 
 SUBFOLDER_TO_PERMALINK = {
     ROOT: 'https://kaylielam03.wixsite.com/myportfolio',
-    'vda': 'https://vda-kaylielam.notion.site/vda-kaylielam/Visual-Design-Apprenticeship-Journal-b3e84cd456114f1aa913d58f4e1d7c17'
+    'vda': 'https://vda-kaylielam.notion.site/vda-kaylielam/Visual-Design-Apprenticeship-Journal-b3e84cd456114f1aa913d58f4e1d7c17',
+    'bb': 'https://arth.website/',
 }
 
 for subfolder, link in SUBFOLDER_TO_PERMALINK.items():
